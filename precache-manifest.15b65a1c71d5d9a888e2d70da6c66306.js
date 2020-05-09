446882fc241c220ffb442a31fe74fc19bf0bedd1
Anthony Reyes <anthonyreyesf@gmail.com>
2020-05-09 13:20:35 -0400

    Updates

---
+++ precache-manifest.15b65a1c71d5d9a888e2d70da6c66306.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9cf3a9e7c47ab054098847c3fc033b9",
+    "url": "/reactdatavisualcovid19/index.html"
+  },
+  {
+    "revision": "84b5af5d160c8d39f987",
+    "url": "/reactdatavisualcovid19/static/css/main.12b9bf7d.chunk.css"
+  },
+  {
+    "revision": "4a88f23c280f7bdcd290",
+    "url": "/reactdatavisualcovid19/static/js/2.96935e1c.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/reactdatavisualcovid19/static/js/2.96935e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b5af5d160c8d39f987",
+    "url": "/reactdatavisualcovid19/static/js/main.4ca2477e.chunk.js"
+  },
+  {
+    "revision": "c7aaca2118dc2c940122",
+    "url": "/reactdatavisualcovid19/static/js/runtime-main.d50798dc.js"
+  }
+]);
